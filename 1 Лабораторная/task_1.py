4766ad3a83ad234dbf86e491416e6c9a3848d208
Dafter <72041616+DafterT@users.noreply.github.com>
2023-10-30 10:44:19 +0300

    Update task_1.py

--- 1 Лабораторная/task_1.py
+++ 1 Лабораторная/task_1.py
@@ -12,7 +12,7 @@ average = sum_ / len(numbers)
 numbers[index_of_none] = average
 print("Измененный список:", numbers)
 
-# Простой цикл с enumerate
+# Простой цикл без enumerate
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
 sum_ = 0
